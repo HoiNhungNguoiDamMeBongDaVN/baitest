155fb8c1526102ae82c20905a443fef576658e3d
kha <khahotravinh2002@gmail.com>
2022-11-20 00:04:39 +0700

    'continues2'

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 
     let number=document.getElementById("sum");
-    var sumNumber=0;
     let arrayNumber=[];
+
+    //code old-------------------------------------------------------------------------------------------------------------
+    // var sumNumber=0;
     // function sum(){
     //     let number1=parseInt(document.getElementById("number1").value);
     //     let number2=parseInt(document.getElementById("number2").value);
@@ -24,15 +26,81 @@
     //     )
     // }
 
+    //code old-------------------------------------------------------------------------------------------------------------
+    // function sum(){
+    //     //get data from dom HTML
+    //     let number1=document.getElementById("number1").value;
+    //     let number2=document.getElementById("number2").value;
+    //     let result=document.querySelector(".result");
+
+
+    //     let num1=reverseString(number1);
+    //     let num2=reverseString(number2);
+    //     let length1=number1.length;
+    //     let length2=number2.length;
+    //     let maxLength=null;
+    //     let stringSum='';
+    //     let resultString='';
+    //     let resultNumber=0;
+       
+
+    //     if (length1>length2) {
+    //         maxLength=length1;
+    //         for (let i = maxLength-1; i >= length2; i--) {
+    //             num2+="0";
+    //         }
+    //     }
+    //     if (length2>length1) {
+    //         maxLength=length2;
+    //         for (let i = maxLength-1; i >= length1; i--) {
+    //             num1+="0";
+    //         }
+           
+    //     }
+    //     if(length1==length2){
+    //         maxLength=length1;
+    //     }
+        
+    //     let numberMemorize=0;
+    //     let sumAll=0;
+
+    //     for (let x = 0; x < maxLength; x++) {
+    //         let sum=0;
+    //         sum=(parseFloat(num1.charAt(x))+parseFloat(num2.charAt(x))+numberMemorize);
+    //         sumAll=sum%10
+    //         numberMemorize=Math.floor(sum/10);
+    //         stringSum+=sumAll.toString();
+    //     }
+    //     if(numberMemorize==1){
+    //         stringSum+="1";
+    //     }
+    //     resultString=reverseString(stringSum);
+    //     resultNumber=parseFloat(resultString)
+
+    //     //render to interface
+    //     arrayNumber.push(resultNumber);
+    //     const setJson=JSON.stringify(arrayNumber);
+    //     localStorage.setItem('key',setJson);
+
+    //     const obj=JSON.parse(localStorage.getItem('key'));
+        
+    //     result.innerHTML=arrayNumber.map(item=>
+    //         `
+    //         <p>Kết quả ${item}</p>
+    //         `
+    //     )
+    // }
+
+    //code new -------------------------------------------------------------------------------------------------------------
     function sum(){
         //get data from dom HTML
-        let number1=document.getElementById("number1").value;
-        let number2=document.getElementById("number2").value;
+        //convert string to array
+        let number1=(document.getElementById("number1").value).split('');
+        let number2=(document.getElementById("number2").value).split('');
+        //------------------
+
         let result=document.querySelector(".result");
-
-
-        let num1=reverseString(number1);
-        let num2=reverseString(number2);
+     
         let length1=number1.length;
         let length2=number2.length;
         let maxLength=null;
@@ -41,46 +109,53 @@
         let resultNumber=0;
        
 
-        if (length1>length2) {
-            maxLength=length1;
-            for (let i = maxLength-1; i >= length2; i--) {
-                num2+="0";
+        if (length1<length2) {
+            maxLength=length2;
+            for (let i = length1; i < maxLength; i++) {
+                // using unshift add "a" to the string
+                number1.unshift("0");
             }
         }
-        if (length2>length1) {
-            maxLength=length2;
-            for (let i = maxLength-1; i >= length1; i--) {
-                num1+="0";
+        if (length2<length1) {
+            maxLength=length1;
+            for (let i = length2; i < maxLength ; i++) {
+                // using unshift add "a" to the string
+                number2.unshift("0");
             }
            
         }
         if(length1==length2){
-            maxLength=length1;
+            maxLength=length2;
         }
-        
+        //convert array to string
+        let num1=number1.join('');
+        let num2=number2.join('');
+        //------------------
         let numberMemorize=0;
         let sumAll=0;
-
-        for (let x = 0; x < maxLength; x++) {
-            let sum=0;
+       
+        
+    
+        for (let x = (maxLength)-1; x >=0; x--) {
+        let sum=0;
             sum=(parseFloat(num1.charAt(x))+parseFloat(num2.charAt(x))+numberMemorize);
-            sumAll=sum%10
+            console.log(sum);
+            sumAll=sum%10;
             numberMemorize=Math.floor(sum/10);
             stringSum+=sumAll.toString();
         }
+      
         if(numberMemorize==1){
             stringSum+="1";
         }
         resultString=reverseString(stringSum);
-        resultNumber=parseFloat(resultString)
+        resultNumber=parseFloat(resultString);
             
         //render to interface
         arrayNumber.push(resultNumber);
         const setJson=JSON.stringify(arrayNumber);
         localStorage.setItem('key',setJson);
-
         const obj=JSON.parse(localStorage.getItem('key'));
-        
         result.innerHTML=arrayNumber.map(item=>
             `
             <p>Kết quả ${item}</p>
